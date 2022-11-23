b5bb63f75418b62eefbe07da047a8db3907ef5e6
Yaël Guilloux <yael.guilloux@gmail.com>
2022-11-23 16:01:08 +0100

    feat(fonts): drop fonts management
    re #2

--- src/module.ts
+++ src/module.ts
@@ -1,21 +1,11 @@
 import { createResolver, defineNuxtModule, installModule } from '@nuxt/kit'
 import type { PinceauOptions } from 'pinceau'
 
-interface ModuleOptions {
-  fonts: string[]
-}
-
-const module: any = defineNuxtModule<ModuleOptions>({
+const module: any = defineNuxtModule({
   meta: {
     name: '@nuxt-themes/tokens',
     configKey: 'tokens',
   },
-  defaults: () => ({
-    fonts: [
-      'https://rsms.me/inter/inter.css',
-      'https://fonts.googleapis.com/css2?family=Fira+Code&display=swaps',
-    ],
-  }),
   async setup(options, nuxt) {
     const modulePath = createResolver(import.meta.url)
 
@@ -39,29 +29,9 @@ const module: any = defineNuxtModule<ModuleOptions>({
       tsConfig.compilerOptions.paths['@nuxt-themes/tokens/config'] = [modulePath.resolve('./tokens.config.ts')]
     })
 
-    // Push fonts from `fonts` keys
-    nuxt.options.app.head = nuxt.options.app.head || {}
-    nuxt.options.app.head.link = nuxt.options.app.head.link || []
-    ;(options?.fonts || []).forEach(
-      (href) => {
-        nuxt.options.app.head.link.push({
-          rel: 'stylesheet',
-          href,
-        })
-      },
-    )
-
-    await installModule('@nuxtjs/color-mode', {
-      classSuffix: '',
-    })
-
-    // Ensure Pinceau is loaded
-    await installModule(
-      'pinceau/nuxt',
-      {
-        configFileName: 'tokens.config',
-      },
-    )
+    await installModule('@nuxtjs/color-mode')
+    
+    await installModule('pinceau/nuxt', { configFileName: 'tokens.config' })
   },
 })
 
