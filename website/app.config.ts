cccc75a1debbcb1bb6e4cee71888914a34f6d0f4
Yaël Guilloux <yael.guilloux@gmail.com>
2022-12-16 01:25:17 +0100

    chore(deps): upgrade deps

--- website/app.config.ts
+++ website/app.config.ts
@@ -1,6 +1,30 @@
 import { defineAppConfig } from 'nuxt/app'
-import proseConfig from '@nuxt-themes/typography/app-config'
 
 export default defineAppConfig({
-  ...proseConfig
+  prose: {
+    copyButton: {
+      iconCopy: 'ph:copy',
+      iconCopied: 'ph:check'
+    },
+    headings: {
+    },
+    h1: {
+      icon: ''
+    },
+    h2: {
+      icon: ''
+    },
+    h3: {
+      icon: ''
+    },
+    h4: {
+      icon: ''
+    },
+    h5: {
+      icon: ''
+    },
+    h6: {
+      icon: ''
+    }
+  }
 })
