cf04d577a1e5bf3a18d51e9e1fe4b6137bf03bd6
Serhii Bedrytskyi <bedrytskyi@gmail.com>
2023-04-13 15:51:35 +0300

    feat: add easing function tokens (#7)

--- src/tokens.config.ts
+++ src/tokens.config.ts
@@ -534,6 +534,56 @@ export default defineTheme({
     },
   },
 
+  ease: {
+    $schema: {},
+    linear: 'cubic-bezier(0, 0, 1, 1)',
+    default: {
+      in: 'cubic-bezier(0.4, 0, 1, 1)',
+      out: 'cubic-bezier(0, 0, 0.2, 1)',
+      inOut: 'cubic-bezier(0.4, 0, 0.2, 1)'
+    },
+    sine: {
+      in: 'cubic-bezier(0.12, 0, 0.39, 0)',
+      out: 'cubic-bezier(0.61, 1, 0.88, 1)',
+      inOut: 'cubic-bezier(0.37, 0, 0.63, 1)'
+    },
+    quad: {
+      in: 'cubic-bezier(0.11, 0, 0.5, 0)',
+      out: 'cubic-bezier(0.5, 1, 0.89, 1)',
+      inOut: 'cubic-bezier(0.45, 0, 0.55, 1)'
+    },
+    cubic: {
+      in: 'cubic-bezier(0.32, 0, 0.67, 0)',
+      out: 'cubic-bezier(0.33, 1, 0.68, 1)',
+      inOut: 'cubic-bezier(0.65, 0, 0.35, 1)'
+    },
+    quart: {
+      in: 'cubic-bezier(0.5, 0, 0.75, 0)',
+      out: 'cubic-bezier(0.25, 1, 0.5, 1)',
+      inOut: 'cubic-bezier(0.76, 0, 0.24, 1)'
+    },
+    quint: {
+      in: 'cubic-bezier(0.64, 0, 0.78, 0)',
+      out: 'cubic-bezier(0.22, 1, 0.36, 1)',
+      inOut: 'cubic-bezier(0.83, 0, 0.17, 1)'
+    },
+    expo: {
+      in: 'cubic-bezier(0.7, 0, 0.84, 0)',
+      out: 'cubic-bezier(0.16, 1, 0.3, 1)',
+      inOut: 'cubic-bezier(0.87, 0, 0.13, 1)'
+    },
+    circ: {
+      in: 'cubic-bezier(0.55, 0, 1, 0.45)',
+      out: 'cubic-bezier(0, 0.55, 0.45, 1)',
+      inOut: 'cubic-bezier(0.85, 0, 0.15, 1)'
+    },
+    back: {
+      in: 'cubic-bezier(0.36, 0, 0.66, -0.56)',
+      out: 'cubic-bezier(0.34, 1.56, 0.64, 1)',
+      inOut: 'cubic-bezier(0.68, -0.6, 0.32, 1.6)'
+    }
+  },
+
   utils: {
     my: (value: PropertyValue<'margin'>) => {
       return {
